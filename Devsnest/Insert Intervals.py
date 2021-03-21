653fba862f87c667d5709c854312b090c17fd03b
Akshansh <s.akshansh01@gmail.com>
2021-03-22 00:07:51 +0530

    22/3

---
+++ Devsnest/Insert Intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        result = []
+        
+        for i in intervals:
+            if i[1] < newInterval[0]:
+                result.append(i)
+                
+            elif i[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = i
+                
+            else:
+                newInterval[0] = min(i[0], newInterval[0])
+                newInterval[1] = max(newInterval[1], i[1])
+                
+        result.append(newInterval); 
+        return result
