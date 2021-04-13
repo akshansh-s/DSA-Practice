95f5829eaef7f9b37a1d503f7ebac93f983023d0
Akshansh <s.akshansh01@gmail.com>
2021-04-13 08:54:38 +0530

    Day 15 Questions

---
+++ Devsnest/Leetcode/DiameterofBinaryTree.py
@@ -0,0 +1,17 @@
+ Definition for a binary tree node.
+ class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+def dia(node):
+    if not node:
+        return 0,0
+    lp,lw=dia(node.left)
+    rp,rw=dia(node.right)
+    
+    return 1+max(lp,rp) , max(lw,rw,1+lp+rp)
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        return dia(root)[1] -1
