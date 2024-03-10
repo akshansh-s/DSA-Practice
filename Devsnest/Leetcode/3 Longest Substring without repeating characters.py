df2fd3d2f3b6fd28a97fac7456be9accad3517a5
Akshansh <36948805+akshansh-s@users.noreply.github.com>
2024-03-10 20:20:50 +0530

    sliding window q

--- Devsnest/Leetcode/3 Longest Substring without repeating characters.py
+++ Devsnest/Leetcode/3 Longest Substring without repeating characters.py
@@ -45,3 +45,23 @@ class Solution:
                 longest=max(longest,len(ch))
             r+=1
         return longest
+    
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charSet = set()
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
+        
+        return maxLength
